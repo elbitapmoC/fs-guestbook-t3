9f15682ad318466419f3db03c587ceb9d5e6390f
Ahkal <bp@sojourn.llc>
2022-12-13 15:31:22 -0500

    Updates: - Removes example from schema - Adds Guesbook - Adds NextAuth, users can log-in and out via Discord - Fixes issues with createContext and createRouter - Removes example - Adds guestbook - Upgrades to dark style

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,6 +1,8 @@
 import { type inferAsyncReturnType } from "@trpc/server";
-import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { type Session } from "next-auth";
+
+import * as trpc from "@trpc/server";
+import type * as trpcNext from "@trpc/server/adapters/next";
 
 import { getServerAuthSession } from "../common/get-server-auth-session";
 import { prisma } from "../db/client";
@@ -25,8 +27,11 @@ export const createContextInner = async (opts: CreateContextOptions) => {
  * This is the actual context you'll use in your router
  * @link https://trpc.io/docs/context
  **/
-export const createContext = async (opts: CreateNextContextOptions) => {
-  const { req, res } = opts;
+export const createContext = async (
+  opts: trpcNext.CreateNextContextOptions
+) => {
+  const req = opts?.req;
+  const res = opts?.res;
 
   // Get the session from the server using the unstable_getServerSession wrapper function
   const session = await getServerAuthSession({ req, res });
@@ -37,3 +42,5 @@ export const createContext = async (opts: CreateNextContextOptions) => {
 };
 
 export type Context = inferAsyncReturnType<typeof createContext>;
+
+export const createRouter = () => trpc.router<Context>();
